391c658b04221af1849c1f1929afdd47cdc30191
descartez <rvanstong@gmail.com>
2015-02-26 11:13:47 -0800

    adds in refCtrl and ability to add in objects to reference container

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@ var app = angular.module('refApp', [])
 
 app.module('MainCtrl', [function() {
   this.references = [];
-
+  var addRef = function(title, url) {
+    references.push(title: title, url: url);
+  };
 }]);
