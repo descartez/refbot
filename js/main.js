06659e9297b9e7a84aa83afe28d871aeebb2968b
descartez <rvanstong@gmail.com>
2015-02-26 11:18:53 -0800

    removes boilerplate readme

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var app = angular.module('refApp', [])
 
 app.module('MainCtrl', [function() {
   this.references = [];
-  var addRef = function(title, url) {
-    references.push(title: title, url: url);
+  var addRef = function(title, url, size) {
+    references.push(title: title, url: url, size: size);
   };
 }]);
