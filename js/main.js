cc2367e8186abec6573d5e07ef595aca57a9f77a
descartez <rvanstong@gmail.com>
2015-02-26 14:51:38 -0800

    begins building rendering process

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ var app = angular.module('refApp', []);
 app.controller('MainCtrl', [function() {
   console.log('MainCtrl has loaded!')
   var self = this
-  self.references = [];
+  self.references = [{size: 6, url:'http://globe-views.com/dcim/dreams/sword/sword-05.jpg'}, {size: 4, url: 'http://a.tgcdn.net/images/products/zoom/f2bb_orcrist_the_sword_of_thorin_oakenshield.jpg'}];
   self.testMessage = 'LOADED'
-  var addRef = function(title, url, size) {
-    var added = {title: title, url: url, size: size}
+  var addRef = function(url, size) {
+    var added = {url: url, size: size}
     console.log('loaded: ' + added)
     references.push(added);
   };
