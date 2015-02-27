7cfd2690144649d6497e3b0ebf40e0dfb349a25f
descartez <rvanstong@gmail.com>
2015-02-26 16:09:29 -0800

    adds in url submission form

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@ var app = angular.module('refApp', []);
 app.controller('MainCtrl', [function() {
   console.log('MainCtrl has loaded!')
   var self = this
-  self.references = [{size: 6, url:'http://globe-views.com/dcim/dreams/sword/sword-05.jpg'}, {size: 4, url: 'http://a.tgcdn.net/images/products/zoom/f2bb_orcrist_the_sword_of_thorin_oakenshield.jpg'}];
+  self.references = [];
   self.testMessage = 'LOADED'
-  var addRef = function(url, size) {
-    var added = {url: url, size: size}
-    console.log('loaded: ' + added)
-    references.push(added);
+  var addRef = function() {
+    console.log('ctrl got here');
+    // var added = {url: url, size: size};
+    // console.log('loaded: ' + added);
+    // self.references.push(added);
   };
 }]);
