e62782a5fc2edc222cf2d291e4e7da342b1418a9
descartez <rvanstong@gmail.com>
2015-02-26 20:20:02 -0800

    finishes urlChecker method

--- js/main.js
+++ js/main.js
@@ -5,8 +5,7 @@ app.controller('MainCtrl', [function() {
   self.references = [];
   // self.columns = 12
   self.addRef = function(url, size) {
-    urlChecker(url);
-    var added = {url: url, size: size};
+    var added = {url: urlChecker(url), size: size};
     self.references.push(added);
     // self.columns -= size;
     // console.log(self.columns)
@@ -16,12 +15,11 @@ app.controller('MainCtrl', [function() {
     //checks if pattern is present
     // if not, adds on http to url
       // what if https needed? error handling?
-      var pattern = /^(https?:\/\/)?/
-      if(!pattern.test(url)) {
+      var pattern = /^(http?:\/\/)?/
+      if(!!pattern.test(url)) {
         url = "http://" + url;
-        console.log('added http')
-      }
-      console.log('urlChecker done')
+      };
+      return url;
     }
 
   }]);
