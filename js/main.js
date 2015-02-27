ecac67d28ff271424a696ae78529d48461e32351
descartez <rvanstong@gmail.com>
2015-02-26 19:36:09 -0800

    adds basic functionality, user can add pictures to board, denote desired size

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 var app = angular.module('refApp', []);
 
 app.controller('MainCtrl', [function() {
-  console.log('MainCtrl has loaded!')
   var self = this
   self.references = [];
-  self.testMessage = 'LOADED'
+  self.columns = 12
+  // self.added = {};
   self.addRef = function(url, size) {
-    console.log('ctrl got here');
     var added = {url: url, size: size};
-    console.log('loaded: ' + added);
-    self.references.push(added.size);
+    self.references.push(added);
+    self.columns -= size;
+    console.log(self.columns)
   };
 }]);
