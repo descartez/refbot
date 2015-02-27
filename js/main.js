2c81da267e6661e15456afb9ab074a805ed1bd39
descartez <rvanstong@gmail.com>
2015-02-26 19:54:39 -0800

    starts urlChecker function

--- js/main.js
+++ js/main.js
@@ -3,12 +3,17 @@ var app = angular.module('refApp', []);
 app.controller('MainCtrl', [function() {
   var self = this
   self.references = [];
-  self.columns = 12
-  // self.added = {};
+  // self.columns = 12
   self.addRef = function(url, size) {
     var added = {url: url, size: size};
     self.references.push(added);
-    self.columns -= size;
+    // self.columns -= size;
     console.log(self.columns)
   };
+
+  var urlChecker = function(url) {
+    //checks if pattern is present
+    // if not, adds on http to url
+  }
+
 }]);
