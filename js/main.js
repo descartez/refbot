7cb8988909b1ee9616a723158cc78450def27929
descartez <rvanstong@gmail.com>
2015-02-26 20:06:50 -0800

    begins regex for urlChecker

--- js/main.js
+++ js/main.js
@@ -5,15 +5,23 @@ app.controller('MainCtrl', [function() {
   self.references = [];
   // self.columns = 12
   self.addRef = function(url, size) {
+    urlChecker(url);
     var added = {url: url, size: size};
     self.references.push(added);
     // self.columns -= size;
-    console.log(self.columns)
+    // console.log(self.columns)
   };
 
   var urlChecker = function(url) {
     //checks if pattern is present
     // if not, adds on http to url
-  }
+      // what if https needed? error handling?
+      var pattern = /^(https?:\/\/)?/
+      if(!pattern.test(url)) {
+        url = "http://" + url;
+        console.log('added http')
+      }
+      console.log('urlChecker done')
+    }
 
-}]);
+  }]);
