e5fa23ef2bba3936b2f18d955f3216e722b7b6ce
descartez <rvanstong@gmail.com>
2015-03-04 16:10:07 -0800

    still working on urlChecker

--- js/main.js
+++ js/main.js
@@ -4,23 +4,23 @@ app.controller('MainCtrl', [function() {
   var self = this
   self.references = [];
   // self.columns = 12
-  self.addRef = function(url, size) {
-    var added = {url: urlChecker(url), size: size};
+  self.addRef = function(urlToPass, size) {
+    var added = {url: urlChecker(urlToPass), size: size};
+    console.log(added);
     self.references.push(added);
     // self.columns -= size;
     // console.log(self.columns)
   };
 
-  var urlChecker = function(url) {
+  var urlChecker = function(urlToTest) {
     //checks if pattern is present
     // if not, adds on http to url
       // what if https needed? error handling?
       var pattern = /^(http?:\/\/)?/
-      if(!!pattern.test(url)) {
-        url = "http://" + url;
+      if(!pattern.test(urlToTest)) {
+        urlToTest = "http://" + urlToTest;
       };
-      // now it won't work UNLESS http is gone
-      return url;
-    }
+      return urlToTest
+    };
 
   }]);
