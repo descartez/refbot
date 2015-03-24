66f6e8ea34a41d30162900ad835d766f638eb607
descartez <rvanstong@gmail.com>
2015-03-24 16:06:06 -0700

    changes very little

--- js/main.js
+++ js/main.js
@@ -3,13 +3,20 @@ var app = angular.module('refApp', []);
 app.controller('MainCtrl', [function() {
   var self = this
   self.references = [];
-  // self.columns = 12
   self.addRef = function(urlToPass, size) {
     var added = {url: urlChecker(urlToPass), size: imgSizer(size)};
     console.log(added);
     self.references.push(added);
-    // self.columns -= size;
-    // console.log(self.columns)
+  };
+
+  self.changeSize = function(object, amount) {
+    if ((object['size'] + amount) > 12) {
+      object['size'] = 12;
+    } else if ((object['size'] + amount) < 2) {
+      object['size'] = 2;
+    } else {
+      object['size'] += amount;
+    }
   };
 
   var urlChecker = function(urlToTest) {
@@ -23,8 +30,11 @@ app.controller('MainCtrl', [function() {
       return urlToTest;
     };
 
+
   var imgSizer = function(size) {
-    if (size === 'small') {
+    if (size === undefined) {
+      return 2;
+    } else if (size === 'small') {
       return 2;
     } else if (size === 'medium') {
       return 3
