2ebb7d46ceec6671dc36a6e5a4d6d2bfdc84362f
descartez <rvanstong@gmail.com>
2015-03-26 15:59:04 -0700

    fixes routes to elim api/api/board route, makes it api/board. added jbuilder gem

--- refbot_api/config/routes.rb
+++ refbot_api/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :api do
+
       resources :board
-    end
+
   end
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -58,4 +59,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
