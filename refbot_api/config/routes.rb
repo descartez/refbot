2da10e7d8b96e94e362c054e9d0178006085be30
descartez <rvanstong@gmail.com>
2015-03-26 16:01:57 -0700

    adds versioning to api routes

--- refbot_api/config/routes.rb
+++ refbot_api/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-
+    namespace :v1 do
       resources :board
-
+    end
   end
 end
   # The priority is based upon order of creation: first created -> highest priority.
