5d4ffb55b6f07060e402e4ddc88fae435d04fea2
descartez <rvanstong@gmail.com>
2015-03-27 16:43:16 -0700

    still twiddling with model methods

--- refbot_api/app/controllers/board_controller.rb
+++ refbot_api/app/controllers/board_controller.rb
@@ -1,7 +1,9 @@
 module Api
   module V1
     class BoardController < ApplicationController
+      def get_tiles
 
+      end
     end
   end
 end
--- refbot_api/app/models/board.rb
+++ refbot_api/app/models/board.rb
@@ -10,5 +10,12 @@ class Board < ActiveRecord::Base
     return "#{Faker::Lorem.word}-#{Faker::Hacker.verb}-#{Faker::Number.number(6)}"
   end
 
+  def get_tiles
+    self.tiles.each do |tile|
+      returned_tiles = []
+      returned_tiles << {url: tile.url, size: tile.size}
+    end
+    return returned_tiles
+  end
 
 end
