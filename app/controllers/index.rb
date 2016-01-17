52d486eed99e07d0f0aab29343c8fbd1d4d6bed4
descartez <rvanstong@gmail.com>
2016-01-17 14:40:07 -0800

    Adds in more to POST route; adds clearBoard function to main.js to prevent board GET from stacking on each other. Call should only call the board, and any other calls should clear the board.
    Still working on POST method. Lots of loops atm, def need to create some helper methods for later. Adds in id to tiles so they can be found by db

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,16 +9,41 @@ end
 
 get '/boards/:id' do
   #gets particular board. MUST TURN DATA INTO JSON OBJECT
-  board = Board.find(params[:id])
-  board_title = board.title
-  tiles = board.tiles
+  # if request.xhr?
+  @board = Board.find(params[:id])
+  board_id = params[:id]
+  board_title = @board.title
+  tiles = @board.tiles
 
-  response = {title: board_title, tiles: tiles}
+  response = {id: board_id, title: board_title, tiles: []}
+  tiles.each do |tile|
+    response[:tiles] << {id: tile.id, url: tile.url, size: tile.size}
+  end
   response.to_json
+  # end
 end
 
-put '/boards/:id' do
+post '/boards/:id' do
   #updates existing board with new tiles
+  # if request.xhr?
+  @board = Board.find(params[:id])
+  if !!@board
+    @board.title = params[:title]
+  else
+    @board = Board.new(title: params[:title])
+  end
+
+  params[:tiles].each do |tile|
+    found_tile = Tile.find(tile[:id])
+    if !!found_tile
+      found_tile.save!
+    else
+      new_tile = Tile.create(url: tile[:url], size: tile[:size])
+      @board.tiles << new_tile
+    end
+  end
+  @board.save
+  # end
 end
 
 delete '/boards/:id' do
--- public/js/main.js
+++ public/js/main.js
@@ -1,63 +1,82 @@
 var app = angular.module('refApp', []);
 
 app.controller('MainCtrl', ["$http",
-    function($http) {
-        var self = this;
-        self.boardTitle = ""
-        self.boardReferences = [];
+  function($http) {
+    var self = this;
+    self.boardTitle = ""
+    self.boardId = 0
+    self.boardReferences = [];
 
-        self.getBoard = function(boardId) {
-            $http({
-                method: 'GET',
-                url: '/boards/' + boardId
-            }).then(function successCallback(response) {
-                console.log(response.data)
-                fullBoard = response.data
-                self.boardTitle = fullBoard.title
+    self.clearBoard = function () {
+      self.boardTitle = ""
+      self.boardId = 0
+      self.boardReferences = [];
+    }
 
-                for (var index = 0; index < fullBoard.tiles.length; index++) {
-                    var tileUrl = fullBoard.tiles[index].tile.url
-                    var tileSize = fullBoard.tiles[index].tile.size
+    self.getBoard = function(boardId) {
+      self.clearBoard();
+      $http({
+        method: 'GET',
+        url: '/boards/' + boardId
+      }).then(function successCallback(response) {
+        console.log(response.data)
+        fullBoard = response.data
+        self.boardTitle = fullBoard.title
+        self.boardId = fullBoard.id
+        console.log(self.boardId)
 
-                    self.addRef(tileUrl, tileSize);
+        for (var index = 0; index < fullBoard.tiles.length; index++) {
+          var tileId = fullBoard.tiles[index].id
+          var tileUrl = fullBoard.tiles[index].url
+          var tileSize = fullBoard.tiles[index].size
 
-                }
-            }, function errorCallback(response) {
-                console.log('board ' + boardId + ' not found');
-                console.log(response);
-            });
-        };
+          self.addRef(tileId, tileUrl, tileSize);
 
-        self.addRef = function(urlToPass, size) {
-            var added = {
-                url: self.urlChecker(urlToPass),
-                size: parseInt(size)
-            };
-            console.log(added);
-            self.boardReferences.push(added);
-        };
+        }
+      }, function errorCallback(response) {
+        console.log('board ' + boardId + ' not found');
+        console.log(response);
+      });
+    };
 
-        self.changeSize = function(object, amount) {
-            if ((object['size'] + amount) > 12) {
-                object['size'] = 12;
-            } else if ((object['size'] + amount) < 2) {
-                object['size'] = 2;
-            } else {
-                object['size'] += amount;
-            }
-        };
+    self.saveBoard = function() {
+      boardObj = {id: self.boardId, title: self.boardTitle, tiles: self.boardReferences}
+      $http.post("/boards/" + self.boardId, boardObj)
+      .then(function (response) {
+        console.log(response)
+      })
+    };
 
-        self.urlChecker = function(urlToTest) {
+    self.addRef = function(refId, urlToPass, size) {
+      var added = {
+        id: refId,
+        url: self.urlChecker(urlToPass),
+        size: parseInt(size)
+      };
+      console.log(added);
+      self.boardReferences.push(added);
+    };
+
+    self.changeSize = function(object, amount) {
+      if ((object['size'] + amount) > 12) {
+        object['size'] = 12;
+      } else if ((object['size'] + amount) < 2) {
+        object['size'] = 2;
+      } else {
+        object['size'] += amount;
+      }
+    };
+
+    self.urlChecker = function(urlToTest) {
             //checks if pattern is present
             // if not, adds on http to url
             // what if https needed? error handling?
             var pattern = /^(f|ht)tps?:\/\//i;
             if (!pattern.test(urlToTest)) {
-                urlToTest = "http://" + urlToTest;
+              urlToTest = "http://" + urlToTest;
             };
             return urlToTest;
-        };
+          };
 
-
-    }
-]);
+        }
+        ]);
